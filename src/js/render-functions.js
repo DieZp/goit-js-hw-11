fe97328b62560e1977353752a5f92b705368a49b
DieZp <diezp93@gmail.com>
2025-10-18 16:47:11 +0100

    up

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -3,6 +3,7 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const gallery = document.querySelector('.gallery');
 const loader = document.querySelector('.span-loader');
+const loadmorebtn = document.querySelector('.form-btn-load');
 const lightbox = new SimpleLightbox('.gallery-link', {
     captions: true,
     captionsData: 'alt',
@@ -11,37 +12,37 @@ const lightbox = new SimpleLightbox('.gallery-link', {
 });
 
 export function createGallery(images) {
-    let galleryStr = "";
-    for (const image of images) {
-        galleryStr += 
-        `<li class="gallery-item">
-            <a class="gallery-link" href="${image.largeImageURL}">
-            <img
-                class="gallery-img"
-                src="${image.webformatURL}"
-                alt="${image.tags}"
-            />
-            <ul class="gallery-desc">
-                <li class="gallery-desc-item">
-                    <span class="gallery-desc-item-name">Likes</span>
-                    <span class="gallery-desc-item-value">${image.likes}</span>
-                </li>
-                <li class="gallery-desc-item">
-                    <span class="gallery-desc-item-name">Views</span>
-                    <span class="gallery-desc-item-value">${image.views}</span>
-                </li>
-                <li class="gallery-desc-item">
-                    <span class="gallery-desc-item-name">Comments</span>
-                    <span class="gallery-desc-item-value">${image.comments}</span>
-                </li>
-                <li class="gallery-desc-item">
-                    <span class="gallery-desc-item-name">Downloads</span>
-                    <span class="gallery-desc-item-value">${image.downloads}</span>
-                </li>
-            </ul>
-            </a>
-        </li>` 
-    };    
+    const galleryStr = images
+        .map(image => {
+            return `<li class="gallery-item">
+                        <a class="gallery-link" href="${image.largeImageURL}">
+                        <img
+                            class="gallery-img"
+                            src="${image.webformatURL}"
+                            alt="${image.tags}"
+                        />
+                        <ul class="gallery-desc">
+                            <li class="gallery-desc-item">
+                                <span class="gallery-desc-item-name">Likes</span>
+                                <span class="gallery-desc-item-value">${image.likes}</span>
+                            </li>
+                            <li class="gallery-desc-item">
+                                <span class="gallery-desc-item-name">Views</span>
+                                <span class="gallery-desc-item-value">${image.views}</span>
+                            </li>
+                            <li class="gallery-desc-item">
+                                <span class="gallery-desc-item-name">Comments</span>
+                                <span class="gallery-desc-item-value">${image.comments}</span>
+                            </li>
+                            <li class="gallery-desc-item">
+                                <span class="gallery-desc-item-name">Downloads</span>
+                                <span class="gallery-desc-item-value">${image.downloads}</span>
+                            </li>
+                        </ul>
+                        </a>
+                    </li>`})
+        .join("");
+
     gallery.insertAdjacentHTML('beforeend', galleryStr);    
     lightbox.refresh();
 }
@@ -56,4 +57,12 @@ export function showLoader() {
 
 export function hideLoader() {    
     loader.classList.remove('loader');
+}
+
+export function showLoadMoreButton() {
+    loadmorebtn.classList.add('is-active');
+}
+
+export function hideLoadMoreButton() {
+    loadmorebtn.classList.remove('is-active');
 }
